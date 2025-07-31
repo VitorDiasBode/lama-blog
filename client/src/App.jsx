de99fbc239683164fd3e029315ba5c3e7c6965da
Vitor <vitordiasalves@gmail.com>
2025-07-31 16:50:20 -0300

    fix corrigi a versão do react-router para a 6.30.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import {
+  createBrowserRouter,
+  RouteProvider,
+  Route
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    
+  }
+]);
+
 function App() {
   return <div>Ola Mundo!</div>;
 }
