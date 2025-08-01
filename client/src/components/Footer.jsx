e8027ae797d04fbbf51af8bcb434de899934573b
Vitor <vitordiasalves@gmail.com>
2025-08-01 19:20:00 -0300

    update: adiciona estilo ao footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Logo from '../img/logo.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt='logo'/>
+      <span>Made with <b>React.js</b>, Thank you Lama Dev</span>
+    </footer>
   )
 }
 
